77f8bd1921fb9bcc615c06b015bca93eedd605bc
yifan <372104922@qq.com>
2022-02-15 10:40:46 +0800

    feat: 走马灯与侧边栏多重菜单

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styles from "./App.module.css";
-import { Footer } from "./components/footer"; 
+import { Footer } from "./components/footer";
 import { Header } from "./components/header";
+import { Carousel } from "./components/carousel";
+import { SideMenu } from "./components/sideMenu";
+import { Row, Col } from "antd";
 
 function App() {
-  return <div className={styles.App}>
-    <Header />
-    <Footer />
-  </div>;
+  return (
+    <div className={styles.App}>
+      <Header />
+      {/* 页面内容 */}
+      <div className={styles["page-content"]}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/sideMenu/SideMenu.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./SideMenu.module.css";
+import { sideMenuList } from "./mockup";
+import { Menu } from "antd";
+import { GifOutlined } from "@ant-design/icons";
+import { spawn } from "child_process";
+
+export const SideMenu: React.FC = () => {
+  return (
+      <Menu mode="vertical" className={styles["side-munu"]}>
+      {sideMenuList.map((m, index) => (
+        <Menu.SubMenu
+          key={`side-menu-${index}`}
+          title={
+            <span>
+              <GifOutlined />
+              {m.title}
+            </span>
+          }
+        >
+          {m.subMenu.map((sm, smindex) => (
+            <Menu.SubMenu
+              key={`sm-menu-${smindex}`}
+              title={
+                <span>
+                  <GifOutlined />
+                  {sm.title}
+                </span>
+              }
+            >
+              {sm.subMenu.map((sms, smsindex) => (
+                <Menu.Item key={`sub-sub-menu-${smsindex}`}>
+                  <span>
+                    <GifOutlined />
+                    {sms}
+                  </span>
+                </Menu.Item>
+              ))}
+            </Menu.SubMenu>
+          ))}
+        </Menu.SubMenu>
+      ))}
+    </Menu>
+  );
+};
