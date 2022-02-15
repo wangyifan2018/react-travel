d08994c37dc9b5fc06fb1875288b7f46ce356127
yifan <372104922@qq.com>
2022-02-15 15:48:21 +0800

    chore: 基础路由使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./App.module.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/signIn" render={() => <h1>登录页面</h1>} />
+          <Route render={() => <h1>404 not found</h1>} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
