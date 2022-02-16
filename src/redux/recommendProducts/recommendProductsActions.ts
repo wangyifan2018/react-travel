9e87ac3e7cfef9ad54e6b15c773b211fe57647ea
yifan <372104922@qq.com>
2022-02-16 15:50:43 +0800

    chore: redux-thunk

--- src/redux/recommendProducts/recommendProductsActions.ts
+++ src/redux/recommendProducts/recommendProductsActions.ts
@@ -1,3 +1,7 @@
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "../store";
+import axios from "axios";
+
 export const FETCH_RECOMMEND_PRODUCTS_START = "FETCH_RECOMMEND_PRODUCTS_START"; // 正在调用推荐信息api
 export const FETCH_RECOMMEND_PRODUCTS_SUCCESS =
   "FETCH_RECOMMEND_PRODUCTS_SUCCESS"; // 推荐信息api调用成功
@@ -46,3 +50,23 @@ export const fetchRecommendProductFailActionCreator = (
     payload: error,
   };
 };
+
+// thunk 可以返回一个函数，而不一定是js对象
+// 在一个thunk action中可以完成一些列连续的action操作
+// 并且可以处理异步逻辑
+// 业务逻辑可以从ui层面挪到这里，代码分层会更清晰
+export const giveMeDataActionCreator =
+  (): ThunkAction<void, RootState, unknown, RecommendProductAction> =>
+  async (dispatch, getState) => {
+    dispatch(fetchRecommendProductStartActionCreator());
+    try {
+      const { data } = await axios.get(
+        "http://123.56.149.216:8080/api/productCollections"
+      );
+      dispatch(fetchRecommendProductSuccessActionCreator(data));
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(fetchRecommendProductFailActionCreator(error.message));
+      }
+    }
+  };
